7861c2e35a513e1bd9c2283d7127f40859dd871b
Muhammadaziz <84275085+Muhammadaziz107@users.noreply.github.com>
2022-01-28 11:18:42 +0500

    add

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,10 +1,50 @@
 import { NavLink } from "react-router-dom";
 import "./home.css";
+import { useRef, useEffect, useState } from "react";
 
 import Category from "../../components/category/category";
 import Restaurants from "../../components/restaurants/restaurants";
 import korzina from "../../assets/images/korzina.png";
+
+import close from "../../assets/images/close.png";
+
 function Home() {
+  const elDiv = useRef(null);
+  const elName = useRef(null);
+  const elAddress = useRef(null);
+  const elPhone = useRef(null);
+  const message = useRef(null);
+
+  const openModal = () => {
+    elDiv.current.classList.add("modal--active");
+  };
+  const closeModal = () => {
+    elDiv.current.classList.remove("modal--active");
+  };
+
+  //new order
+
+  async function handleCreateNew(evt) {
+    evt.preventDefault();
+
+    const res = await fetch("http://localhost:4000/newOrder", {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({
+        name: elName.current.value,
+        address: elAddress.current.value,
+        phone: elPhone.current.value,
+      }),
+    });
+
+    if (res.status === 200) {
+      message.current.textContent = "created successfully✅";
+    }
+  }
+
   return (
     <>
       <div>
@@ -15,8 +55,37 @@ function Home() {
             <p className="korzina__text">korzina</p>
           </NavLink>
         </div>
+        <div>
+          <button type="button" className="orders" onClick={openModal}>
+            send order
+          </button>
+        </div>
+
         <Category />
         <Restaurants />
+      </div>
+
+      {/* modal */}
+
+      <div className="modal" ref={elDiv}>
+        <form className="form" onSubmit={handleCreateNew}>
+          <button className="form__close" onClick={closeModal} type="button">
+            <img src={close} alt="close btn" width={20} height={20} />
+          </button>
+          <h2 className="form__heading">Send Order</h2>
+          <h3 ref={message}></h3>
+          <input className="form__input" type="text" placeholder="name" ref={elName} />
+          <input
+            className="form__input"
+            type="text"
+            placeholder="address"
+            ref={elAddress}
+          />
+          <input className="form__input" type="text" placeholder="phone" ref={elPhone} />
+          <button className="form__send" type="submit">
+            Submit
+          </button>
+        </form>
       </div>
     </>
   );
